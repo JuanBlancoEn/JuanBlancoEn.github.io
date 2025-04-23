a4799cd87cb255d50ac9ad415dcfddbaf3d2e26b
JuanBlancoEn <juanengeniosblanco@gmail.com>
2025-04-23 20:25:09 +0200

    Acabada la informacion de naves y pilotos y cambios en el navbar con logo cerca de acabar el json de upgrades

--- team-builder/src/components/Navbar.jsx
+++ team-builder/src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '/r2d2logo.jpeg';
+
 
 function Navbar() {
   return (
     <nav className="navbar bg-dark border-bottom border-body sticky-top" data-bs-theme="dark">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">R2 X-Wing Builder</Link>
-        <div className="navbar-nav">
-          <Link to="/" className="nav-link">Inicio</Link>
-          <Link to="/create" className="nav-link">Crear Equipo</Link>
-          <li className="nav-item">
-              <Link className="nav-link" to="/import">Importar Equipo</Link> {/* Enlace a la página de importar equipo */}
-            </li>
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <Link to="/" className="navbar-brand">
+        <img
+          src={logo}
+            alt="R2 X-Wing Builder"
+            style={{
+              height: "60px",
+              width: "60px",
+              objectFit: "cover",
+              borderRadius: "50%",
+              boxShadow: "0 10px 10px rgba(0, 0, 0, 0.4)"
+            }}
+        />
+        </Link>
+
+        <div className="d-flex gap-3">
+          <Link to="/" className="nav-link text-white">Inicio</Link>
+          <Link to="/create" className="nav-link text-white">Crear Equipo</Link>
+          <Link to="/import" className="nav-link text-white">Importar Equipo</Link>
         </div>
       </div>
     </nav>
