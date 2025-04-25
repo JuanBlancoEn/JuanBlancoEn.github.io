ca27d9b5fc328b4f9354279f03134910163557b6
Juan Engenios Blanco <juanengeniosblanco@gmail.com>
2025-04-25 15:14:16 +0200

    Logo en contactanos y eliminado json inutilizado

--- team-builder/src/pages/InfoPage.jsx
+++ team-builder/src/pages/InfoPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '/r2d2logo.jpeg';  // Asegúrate de colocar la ruta correcta de tu logo
 
 function InfoPage() {
   return (
     <div className="d-flex justify-content-center align-items-center background" style={{ minHeight: '100vh' }}>
       <div className="container-fluid p-4 rounded shadow" style={{ maxWidth: '1000px' }}>
+        
+        {/* Sección con el logo */}
         <div className="text-center">
+          <img src={logo} alt="Logo de la Asociación R2D2 X-Wing" style={{ width: '150px', marginBottom: '20px' }} />  {/* Logo con tamaño ajustable */}
           <h1 className="mb-4">R2D2 X-Wing</h1>
           <p className="lead">
             ¡Bienvenido a nuestra página de contacto! Si tienes alguna pregunta, no dudes en ponerte en contacto con nosotros.
@@ -16,7 +20,7 @@ function InfoPage() {
           <div className="col-md-6">
             <h3>Información de contacto</h3>
             <ul className="list-unstyled">
-            <li><strong>Email:</strong> <a href="mailto:r2d2xwinggrupo@gmail.com">r2d2xwinggrupo@gmail.com</a></li> {/* Enlace mailto */}
+              <li><strong>Email:</strong> <a href="mailto:r2d2xwinggrupo@gmail.com">r2d2xwinggrupo@gmail.com</a></li> {/* Enlace mailto */}
             </ul>
           </div>
           
@@ -36,13 +40,10 @@ function InfoPage() {
         </div>
 
         <footer className="text-center mt-5">
-          <p>&copy; {new Date().getFullYear()} <Link to="https://github.com/JuanBlancoEn" target="_blank">JuanBlancoEn</Link>.  Proyecto de código abierto.</p>
+          <p>&copy; {new Date().getFullYear()} <Link to="https://github.com/JuanBlancoEn" target="_blank">JuanBlancoEn</Link>. Proyecto de código abierto.</p>
         </footer>
       </div>   
     </div>
-    
-    
-
   );
 }
 
